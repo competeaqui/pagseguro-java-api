316e502815d16e4c1d56cbc9633d4ce725554c79
Manoel Campos <manoelcampos@gmail.com>
2024-01-12 18:47:19 -0300

    Adiciona construtor no Item que não recebe o reference_id e define quantidade igual a 1.
    Signed-off-by: Manoel Campos <manoelcampos@gmail.com>

--- src/main/java/br/com/competeaqui/pagseguro/data/Item.java
+++ src/main/java/br/com/competeaqui/pagseguro/data/Item.java
@@ -5,15 +5,26 @@ import lombok.NonNull;
 
 /**
  * Item de uma venda/pedido.
- * @param name Nome (descrição) do item.
- * @param quantity Quantidade do item no pedido.
- * @param unit_amount Valor unitário em centavos, sendo o valor mínimo 100 centavos (R$ 1,00)
  *
+ * @param name        Nome (descrição) do item.
+ * @param unit_amount Valor unitário em centavos, sendo o valor mínimo 100 centavos (R$ 1,00)
+ * @param quantity    Quantidade do item no pedido.
  * @author Manoel Campos da Silva Filho
  * @see PixOrder
  */
-public record Item(@NonNull String name, int quantity, int unit_amount, String reference_id) {
-    public Item(final String name, final int quantity, final int unit_amount){
-        this(name, quantity, unit_amount, null);
+public record Item(@NonNull String name, int unit_amount, int quantity, String reference_id) {
+    /**
+     * Cria um item sem um reference_id e com quantidade igual a 1.
+     */
+    public Item(final String name, final int unit_amount){
+        this(name, unit_amount, 1, null);
     }
+
+    /**
+     * Cria um item sem um reference_id.
+     */
+    public Item(final String name, final int unit_amount, final int quantity){
+        this(name, unit_amount, quantity, null);
+    }
+
 }
